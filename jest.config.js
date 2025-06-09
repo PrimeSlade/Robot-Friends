df3d14063b8f7d7611e1bc15e9d0965f7acf0989
Slade <saib6064@gmail.com>
2025-06-09 17:17:47 +0700

    feat: configure jest for react testing with typescript
    This commit configures Jest for React testing with TypeScript,
    including:
    
    - Setting the test environment to jsdom.
    - Configuring Babel to transform JavaScript and TypeScript files.
    - Mocking CSS imports.
    - Setting up module file extensions.
    - Adding test match pattern.
    - Enabling verbose output.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
-// jest.config.js
-export const testEnvironment = "jest-environment-jsdom";
-export const setupFilesAfterEnv = ["<rootDir>/jest.setup.js"];
+export default {
+  testEnvironment: "jest-environment-jsdom",
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+
+  moduleNameMapper: {
+    // Mock all CSS imports
+    "\\.(css|scss|sass|less)$": "<rootDir>/test/__mocks__/styleMock.js",
+  },
+
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
+
+  testMatch: ["**/test/**/*.test.[jt]s?(x)"],
+
+  verbose: true,
+};
