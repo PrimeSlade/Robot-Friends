a3c193c61b3b5817d351d123d3786959e5c2c471
Slade <saib6064@gmail.com>
2025-06-11 12:07:41 +0700

    feat(test): add tests for App component and filteredRobots util test(App.test.jsx): add snapshot test for App component test(App.test.jsx): add test for filteredRobots util function

--- test/containers/App.test.jsx
+++ test/containers/App.test.jsx
@@ -1,12 +1,27 @@
 import { render, waitFor } from "@testing-library/react";
 
 import App from "../../src/containers/App";
+import { filteredRobots } from "../../src/utils/filteredRobots";
+import { robots } from "../../src/robots";
 
-test("testing app by using snapshot", async () => {
-  let asFragment;
-  await waitFor(async () => {
-    const renderResult = render(<App />);
-    asFragment = renderResult.asFragment;
+describe("App Component and utils tests", () => {
+  test("testing app by using snapshot", async () => {
+    let asFragment;
+    await waitFor(async () => {
+      const renderResult = render(<App />);
+      asFragment = renderResult.asFragment;
+    });
+    expect(asFragment()).toMatchSnapshot();
   });
-  expect(asFragment()).toMatchSnapshot();
+
+  test("testing filtered func", () => {
+    const mockRobots = [
+      { id: 1, name: "Robot One", email: "one@robots.com" },
+      { id: 2, name: "Robot Two", email: "two@robots.com" },
+      { id: 3, name: "Robot Three", email: "three@robots.com" },
+    ];
+
+    expect(filteredRobots(robots, "")).toEqual(robots);
+    expect(filteredRobots(mockRobots, "one")).toEqual([mockRobots[0]]);
+  });
 });
