ee02a20cacc24d669f0ae2e67f468e8217ec7a8d
Slade <saib6064@gmail.com>
2025-06-12 12:37:10 +0700

    test(CardList.test.jsx): add rendering logic tests for CardList component
    This commit adds tests to verify the rendering of the CardList
    component, ensuring that the correct number of cards are rendered
    and that the names of the robots are displayed correctly.
    A data-testid was added to the Card component to be able to test it.

--- test/components/CardList.test.jsx
+++ test/components/CardList.test.jsx
@@ -1,20 +1,37 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CardList from "../../src/components/CardList";
+
+const mockRobots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    email: "incere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "Shanna@melissa.tv",
+  },
+];
 
 test("testing CardList by using snapshot", () => {
   // Mock robot data for testing
-  const mockRobots = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      email: "incere@april.biz",
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      email: "Shanna@melissa.tv",
-    },
-  ];
   const { asFragment } = render(<CardList robots={mockRobots} />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+describe("CardList rendering logic", () => {
+  beforeEach(() => {
+    render(<CardList robots={mockRobots} />);
+  });
+
+  test("testing cards", () => {
+    expect(screen.getAllByTestId("card")).toHaveLength(mockRobots.length);
+  });
+
+  test("testing rendered names", () => {
+    mockRobots.forEach((r) => {
+      expect(screen.getByText(r.name)).toBeInTheDocument();
+    });
+  });
 });
