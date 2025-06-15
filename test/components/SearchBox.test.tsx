38002f2cb4f72544409b8a7778705585935ea8e5
Slade <saib6064@gmail.com>
2025-06-15 12:33:16 +0700

    feat(SearchBox.test.tsx): add test case to check if the searchbox is working correctly

--- test/components/SearchBox.test.tsx
+++ test/components/SearchBox.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SearchBox from "../../src/components/SearchBox";
 import React from "react";
 
@@ -6,4 +6,9 @@ test("testing searchBox", () => {
   const mockSearchField = jest.fn();
 
   render(<SearchBox setSearchField={mockSearchField} />);
+
+  const input = screen.getByPlaceholderText("search robots");
+  fireEvent.change(input, { target: { value: "rObot" } }); //e.target.value
+
+  expect(mockSearchField).toHaveBeenCalledWith("robot");
 });
